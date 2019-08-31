0d646015109a5bdf13261ec139fb0c3309b4d12a
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2019-08-31 19:21:48 -0300

    first commit

---
+++ main.hs
@@ -0,0 +1,11 @@
+-- movePeca::Peca peca->Ponto destino->Boll podeMover 
+
+-- imprimeMovimentosPossiveis::Peca peca->[Ponto]
+
+-- imprimeTabuleiro::Matriz tabuleiro[][]->void
+
+-- tranformaEmDama::Peca peca
+
+-- ehDama::Peca peca-> Bool retorno
+
+-- fimDoJogo:: ? -> Bool
