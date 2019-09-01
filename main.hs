d9c47c91cec06ccc9b51ca70919de1b6d04f4754
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2019-09-01 08:09:31 -0300

    Creating main and menu

--- main.hs
+++ main.hs
@@ -1,11 +1,41 @@
--- movePeca::Peca peca->Ponto destino->Boll podeMover 
+import Control.Exception
+import System.IO
+import System.IO.Error
+import System.Process
 
--- imprimeMovimentosPossiveis::Peca peca->[Ponto]
+type EhDama = Bool --Informa se a peça já se tornou dama
+type Posicao = (Int, Int) --Localização da peca na matriz
 
--- imprimeTabuleiro::Matriz tabuleiro[][]->void
+data Peca = Peca Jogador Posicao EhDama
+type QtdPecas = Int
+type Nome = String
+data Jogador = Jogador Nome QtdPecas deriving (Show)
 
--- tranformaEmDama::Peca peca
+menu::IO()
+menu = do
+    system "clear" --Limpa a tela (Apenas para Ubuntu)
+    putStrLn "---------------------------------- Jogo de Dama ----------------------------------"
+    putStr "\n\nDigite 1 para jogar"
+    putStr "\nDigite 0 para sair\n\n"
+    op <- getChar
+    getChar -- descarta o Enter
+    executar op
+    return ()
 
--- ehDama::Peca peca-> Bool retorno
+executar::Char->IO()
+executar '1' = do
+    putStrLn "Jogando"
+    return ()
 
--- fimDoJogo:: ? -> Bool
+executar '0' = do
+    return ()
+
+executar _ = do
+    putStrLn ("\nOpção inválida! Tente novamente")
+    putStr "\nPressione <Enter> para voltar ao menu..."
+    getChar
+    menu
+
+main::IO()
+main = do
+    menu
