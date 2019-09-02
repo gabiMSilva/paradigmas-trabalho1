88af2270bd5db17f4c8ecdb2c69a788a01f96f1b
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2019-09-01 23:53:53 -0300

    Adding register user function

--- main.hs
+++ main.hs
@@ -1,15 +1,27 @@
-import Control.Exception
 import System.IO
-import System.IO.Error
 import System.Process
+
+type Tabuleiro = [Int]
+type Jogadores = [Jogador]
 
 type EhDama = Bool --Informa se a peça já se tornou dama
 type Posicao = (Int, Int) --Localização da peca na matriz
+data Peca = Peca Jogador Posicao EhDama deriving (Show)
 
-data Peca = Peca Jogador Posicao EhDama
 type QtdPecas = Int
 type Nome = String
-data Jogador = Jogador Nome QtdPecas deriving (Show)
+type Id = Int
+data Jogador = Jogador Id Nome QtdPecas deriving (Show)
+
+main::IO()
+main = do
+    menu
+
+getString::String->IO String
+getString str = do
+    putStr str
+    res <- getLine
+    return res
 
 menu::IO()
 menu = do
@@ -24,7 +36,12 @@ menu = do
 
 executar::Char->IO()
 executar '1' = do
-    putStrLn "Jogando"
+   
+    --Cadastra os jogadores
+    jogadores <- cadastraJogador 1 []
+    jogadores <- cadastraJogador 2 jogadores
+    --Inicia um novo jogo
+    novoJogo jogadores
     return ()
 
 executar '0' = do
@@ -32,10 +49,28 @@ executar '0' = do
 
 executar _ = do
     putStrLn ("\nOpção inválida! Tente novamente")
-    putStr "\nPressione <Enter> para voltar ao menu..."
+    putStr "\nPressione <Enter> para voltar ao menu\n"
     getChar
     menu
 
-main::IO()
-main = do
-    menu
+cadastraJogador::Int->Jogadores->IO Jogadores
+cadastraJogador id jogadores = do
+    nome<-getString("Digite o nome do jogador " ++ show id ++ "\n")
+    
+    return ((Jogador id nome 12):jogadores)
+
+novoJogo::Jogadores->IO()
+novoJogo jogadores = do
+    system("clear")
+    putStrLn("Os 1's equivalem às peças do Jogador 1 e os 2's às do Jogador 2")
+    -- Comeca com o primeiro jogador
+    novaPartida 1 [1..64]
+
+novaPartida::Int->Tabuleiro->IO()
+novaPartida id tabuleiro = do
+    system("clear")
+    putStrLn("Vez do Jogador " ++ show id)
+--    imprimeTabuleiro tabuleiro
+
+--imprimeTabuleiro::Tabuleiro->IO()
+--imprimeTabuleiro tabuleiro
